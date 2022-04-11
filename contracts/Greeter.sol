921dda058f93ea2600171a9f181af4f16addc2a8
Dhaesha M <dhaesha1@gmail.com>
2022-04-11 10:03:39 -0400

    addition function added just to test for fun

--- contracts/Greeter.sol
+++ contracts/Greeter.sol
@@ -19,4 +19,9 @@ contract Greeter {
         console.log("Changing greeting from '%s' to '%s'", greeting, _greeting);
         greeting = _greeting;
     }
+
+    function add(uint256 a, uint256 b) public pure returns (uint256) {
+        return a + b;
+    }
+    
 }
