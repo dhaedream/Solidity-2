968d49f5f9f813153aa56fce1a7ffb84c8ed290b
Dhaesha M <dhaesha1@gmail.com>
2022-04-17 23:34:00 -0400

    final public view function that returns current message string value. its wierd to not use storage keyword with string data type in solidity but this is me following a tutorial

--- contracts/Inbox.sol
+++ contracts/Inbox.sol
@@ -4,10 +4,13 @@ pragma solidity ^0.8.0;
 contract Inbox {
     string public message;
 
-    function Inbox(string initialMessage) public {
+    function inbox(string initialMessage) public {
         message = initialMessage;
     }
     function setMessage(string newMessage) public {
         message = newMessage;
+    }
+    function getMessage() public view returns (string) {
+        return message;
     }
 }
